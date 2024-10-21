9378fa2bc93682d9d295d7a662585b050e342bf4
Mateus Robers <robersmateus@gmail.com>
2024-10-21 15:53:58 -0300

    a

--- src/Retangulo.java
+++ src/Retangulo.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
 public class Retangulo {
     private int base, altura;
-    
+    Scanner scanner = new Scanner(System.in);
     
     public Retangulo(int b, int a) {
         base = b;
         altura = a;
+    }
+
+    public Retangulo() {
+        System.out.println("Informe a base: ");
+        base = scanner.nextInt();
+        System.out.println("Informe a altura: ");
+        altura = scanner.nextInt();
     }
 
     public int retornaBase() {
